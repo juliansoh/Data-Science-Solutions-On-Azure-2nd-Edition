00e4f171bab58790914153e574c92542dc6197ab
Julian <31254706+juliansoh@users.noreply.github.com>
2024-06-02 11:31:00 -0700

    Adding files for exercise 8-2
    The LangChain exercise.

---
+++ Chapter-8/exercises/exercise-8-2/serve.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from typing import List
+
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import AzureChatOpenAI
+from langserve import add_routes
+
+import os
+
+os.environ["AZURE_OPENAI_API_KEY"] = "c2a213e68f7f45bca6b7940ba914f191"
+os.environ["AZURE_OPENAI_ENDPOINT"] = "https://ds-2nd-ed.openai.azure.com/"
+os.environ["AZURE_OPENAI_API_VERSION"] = "2024-02-01"
+os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"] = "gpt-35"
+
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{user_message}')
+])
+
+# 2. Create model
+model = AzureChatOpenAI(            
+            temperature=0,
+            deployment_name=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+            azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+            openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+            openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
+            streaming=True,)
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+
+# 4. App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+# 5. Adding chain route
+
+add_routes(
+    app,
+    chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
