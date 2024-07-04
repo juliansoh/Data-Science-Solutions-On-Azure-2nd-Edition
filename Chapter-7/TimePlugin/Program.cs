f1e979d8f7469de298f1564233af5669f6fc6070
Julian <31254706+juliansoh@users.noreply.github.com>
2024-07-03 20:01:54 -0700

    Pushing exercise-7-3

---
+++ Chapter-7/TimePlugin/Program.cs
@@ -0,0 +1,87 @@
+﻿﻿// Copyright (c) Microsoft. All rights reserved.
+#pragma warning disable VSTHRD111 // Use ConfigureAwait(bool)
+#pragma warning disable CA1050 // Declare types in namespaces
+#pragma warning disable CA2007 // Consider calling ConfigureAwait on the awaited task
+
+using System.ComponentModel;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+//Define variables to hold API endpoint, deployment name, and api key
+var azureEndpoint = "https://jsopenai-east.openai.azure.com/";
+var model = "gpt-4-0125";
+var apiKey = "75b6983f87db4926ab950aa947fba9c3";
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build()
+    ?? throw new InvalidOperationException("Configuration is not provided.");
+
+ArgumentNullException.ThrowIfNull(config["OpenAI:ChatModelId"], "OpenAI:ChatModelId");
+ArgumentNullException.ThrowIfNull(config["OpenAI:ApiKey"], "OpenAI:ApiKey");
+
+//var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(
+//    modelId: config["OpenAI:ChatModelId"]!,
+//    AzureEndpoint: config["OpenAI:AzureEndpoint"]!,
+//    apiKey: config["OpenAI:ApiKey"]!);
+var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(model, azureEndpoint, apiKey);
+
+kernelBuilder.Plugins.AddFromType<TimeInformationPlugin>();
+kernelBuilder.Plugins.AddFromType<InventoryInformationPlugin>();
+var kernel = kernelBuilder.Build();
+
+// Get chat completion service
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Enable auto function calling
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+Console.WriteLine("Ask questions to use the Time Plugin such as:\n" +
+                  "- What time is it?");
+
+ChatHistory chatHistory = [];
+string? input = null;
+while (true)
+{
+    Console.Write("\nUser > ");
+    input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        // Leaves if the user hit enter without typing any word
+        break;
+    }
+    chatHistory.AddUserMessage(input);
+    var chatResult = await chatCompletionService.GetChatMessageContentAsync(chatHistory, openAIPromptExecutionSettings, kernel);
+    Console.Write($"\nAssistant > {chatResult}\n");
+}
+
+/// <summary>
+/// A plugin that returns the current time.
+/// </summary>
+public class TimeInformationPlugin
+{
+    /// <summary>
+    /// Retrieves the current time in UTC.
+    /// </summary>
+    /// <returns>The current time in UTC. </returns>
+    [KernelFunction, Description("Retrieves the current time in UTC.")]
+    public string GetCurrentUtcTime()
+        => DateTime.UtcNow.ToString("R");
+}
+
+public class InventoryInformationPlugin
+{
+    /// <summary>
+    /// Retrieves the current time in UTC.
+    /// </summary>
+    /// <returns>The current time in UTC. </returns>
+    [KernelFunction, Description("Lists the current inventory.")]
+    public string ListInventory()
+        => "Inventory: 10 Giro helmet Medium, 5 Giro helmet Large";
+}
---
+++ Chapter-7/exercise-7-3-working/Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Kernel = Microsoft.SemanticKernel.Kernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+//Define variables to hold API endpoint, deployment name, and api key
+var azureEndpoint = "https://jsopenai-east.openai.azure.com/";
+var model = "gpt-4-0125";
+var apiKey = "75b6983f87db4926ab950aa947fba9c3";
+
+// Inject your logger 
+// see Microsoft.Extensions.Logging.ILogger @ https://learn.microsoft.com/dotnet/core/extensions/logging
+ILoggerFactory myLoggerFactory = NullLoggerFactory.Instance;
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddSingleton(myLoggerFactory);
+
+//var kernel = builder.Build();
+//var builder = Kernel.CreateBuilder();
+
+// Configure AI backend used by the kernel
+builder.AddAzureOpenAIChatCompletion(model, azureEndpoint, apiKey);
+
+var kernel = builder.Build();
+
+// MarketingEmail directory path
+var MarketingEmailPluginDirectoryPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "prompt_template_samples", "EmailPlugin");
+var InventoryPluginDirectoryPath = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "prompt_template_samples", "InventoryPlugins");
+
+// Load the MarketingEmailplugin from the Plugins Directory
+var MarketingEmailPluginFunctions = kernel.ImportPluginFromPromptDirectory(MarketingEmailPluginDirectoryPath);
+var InventoryPluginFunctions = kernel.ImportPluginFromPromptDirectory(InventoryPluginDirectoryPath);
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// 2. Enable automatic function calling
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+//create a cart using GUID for this session
+var cartId = Guid.NewGuid().ToString();
+
+var history = new ChatHistory();
+
+//Add this cartID to this sesson as part of the history
+history.AddUserMessage(cartId);
+
+string? userInput;
+do {
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(userInput))
+    {
+        // Leaves if the user hit enter without typing any word
+        break;
+    } 
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // 3. Get the response from the AI with automatic function calling
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
